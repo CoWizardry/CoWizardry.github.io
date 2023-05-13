beb4e75c06ca7669c93fc4dee81ba467822e9c66
CodeWizardry <3112497008@qq.com>
2023-05-13 21:26:56 +0800

    Site updated: 2023-05-13 21:26:55

---
+++ js/weather.js
@@ -0,0 +1,61 @@
+// WIDGET = {
+//     "CONFIG": {
+//       "modules": "12034",
+//       "background": "5",
+//       "tmpColor": "000000",
+//       "tmpSize": "16",
+//       "cityColor": "000000",
+//       "citySize": "16",
+//       "aqiColor": "000000",
+//       "aqiSize": "16",
+//       "weatherIconSize": "24",
+//       "alertIconSize": "18",
+//       "padding": "10px 10px 10px 10px",
+//       "shadow": "0",
+//       "language": "auto",
+//       "borderRadius": "5",
+//       "fixed": "false",
+//       "vertical": "top",
+//       "horizontal": "center",
+//       // "left": "300",
+//       // "top": "100",
+//       "key": "59fe58b6b1204451bd701c0918dac0f1"
+//     }
+// }
+
+
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "17",
+    "cityColor": "FFFFFF",
+    "citySize": "17",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "17",
+    "weatherIconSize": "25",
+    "alertIconSize": "19",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "18",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "57fdd71c0dbc4d168d15b1c60a9c186e"
+  }
+}
+
+// (function(a,h,g,f,e,d,c,b){b=function(){d=h.createElement(g);c=h.getElementsByTagName(g)[0];d.src=e;d.charset="utf-8";d.async=1;c.parentNode.insertBefore(d,c)};a["SeniverseWeatherWidgetObject"]=f;a[f]||(a[f]=function(){(a[f].q=a[f].q||[]).push(arguments)});a[f].l=+new Date();if(a.attachEvent){a.attachEvent("onload",b)}else{a.addEventListener("load",b,false)}}(window,document,"script","SeniverseWeatherWidget","//cdn.sencdn.com/widget2/static/js/bundle.js?t="+parseInt((new Date().getTime() / 100000000).toString(),10)));
+// window.SeniverseWeatherWidget('show', {
+//   flavor: "slim",
+//   location: "WS2V148YRZ56",
+//   geolocation: true,
+//   language: "auto",
+//   unit: "c",
+//   theme: "auto",
+//   token: "8e5a8555-6732-4d55-b19f-dd285802e9a7",
+//   hover: "enabled",
+//   container: "tp-weather-widget"
+// })
